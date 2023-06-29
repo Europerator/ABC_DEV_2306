2cd9c1f7bad604fe79c468f36b3512c459e74f7f
LEFEVRE Maxime <MLefevre@arfp.asso.fr>
2023-06-29 09:26:03 +0200

    Premier exercice en java

---
+++ Algo-Java/Exemple/src/exercicePremier/PremierPremier.java
@@ -0,0 +1,23 @@
+package exercicePremier;
+
+import java.util.Scanner;
+
+public class PremierPremier {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	int firstNumber;
+	int secondNumber;
+	double result;
+	Scanner reader = new Scanner(System.in);
+	
+	System.out.println("Programme de calcul de la moyenne de deux nombres.");
+	System.out.println("Veuillez saisir un premier nombre : ");
+	firstNumber = reader.nextInt();
+	System.out.println("Veuillez saisir un second nombre : ");
+	secondNumber = reader.nextInt();
+	result = (firstNumber + secondNumber) / 2;
+	System.out.println("La moyenne de " + Integer.toString(firstNumber) + " et " + Integer.toString(secondNumber) + " est de : " + Double.toString(result));
+	}
+
+}
