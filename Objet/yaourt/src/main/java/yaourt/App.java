cd4b0878edf9510cf38c38ab4b33eeda536aaa1c
LEFEVRE Maxime <MLefevre@arfp.asso.fr>
2023-09-20 16:46:42 +0200

    commentaires yaourt

--- Objet/yaourt/src/main/java/yaourt/App.java
+++ Objet/yaourt/src/main/java/yaourt/App.java
@@ -11,17 +11,25 @@ import com.google.gson.Gson;
 
 public class App {
 
-	public static void main(String[] args) throws Exception { //throws Exception
+	public static void main(String[] args) throws Exception { //throws Exception sert à éviter d'avoir à gérer un try..catch (j'ai pas envie)
+		
+		//On crée une nouvelle requête HTTP (Get avec l'URL et en précisant qu'on attend une String en réponse)
 		HttpRequest getRequest = HttpRequest.newBuilder()
 				.uri(new URI("https://api.devoldere.net/polls/yoghurts/"))
 				.build();
 		HttpClient httpClient = HttpClient.newHttpClient();
 		HttpResponse<String> getResponse = httpClient.send(getRequest, BodyHandlers.ofString());
+		
+		//Le tour de magie Gson commence :
 		Gson gson = new Gson();
+		//On demande à Gson de créer un objet Yoghurt à partir des champs renseignés dans le json reçu. Il définit les valeurs de chaque attribut en fonction des champs renseignés dans le json.
 		Yoghurts yoghurt = gson.fromJson(getResponse.body(), Yoghurts.class);
-		System.out.println("Resultats du sondage :");
+		
+		//Affichage :
+		System.out.println("Resultats du sondage : " + yoghurt.getPoll());
 		ArrayList<String> options = yoghurt.getOptions();
 		int[] resultat = yoghurt.getScore();
+		//Simple détermination des deux valeurs maximales.
 		String[] vainqueurs = new String[] {"none", "none"};
 		int max[] = new int[] {0, 0};
 		for (int i = 0; i < options.size(); i++) {
@@ -37,7 +45,7 @@ public class App {
 				vainqueurs[1] = options.get(i);
 			}
 		}
-		System.out.println("Les deux couleurs les plus plebiscitees sont : " + vainqueurs[0] + " avec " + max[0] + " votes et " + vainqueurs[1] + " avec " + max[1] + " votes.");
+		System.out.println("Les deux réponses les plus plebiscitées sont : " + vainqueurs[0] + " avec " + max[0] + " votes et " + vainqueurs[1] + " avec " + max[1] + " votes.");
 	}
 
 }
--- Objet/yaourt/src/main/java/yaourt/Yoghurts.java
+++ Objet/yaourt/src/main/java/yaourt/Yoghurts.java
@@ -3,22 +3,36 @@ package yaourt;
 import java.util.ArrayList;
 
 public class Yoghurts {
+	// pour utiliser Gson.fromJson() on utilise des attributs nommés et typés de façon identique aux champs du json reçu : poll (String) , votes (int) , results (String[]). 
 	private String poll;
 	private int votes;
 	private String[] results;
+	//introduction d'un tableau score pour refléter le score de chaque option
 	private int[] score;
 	
+	/*
+	 * Accesseur qui permet de lister les différentes options de results.
+	 */
 	public ArrayList<String> getOptions() {
+		//On utilise ArrayList parce qu'on a la flemme de gérer un tableau avec une taille dynamique (on pourrait améliorer la méthode en retournant un vrai tableau cela dit)
 		ArrayList<String> options = new ArrayList<String>();
+		//Pour chaque entrée de results : si ce n'est pas contenu dans options, le rajouter.
 		for (int i = 0; i < votes; i++) { if (!options.contains(results[i])) { options.add(results[i]); } }
+		//Ici on pourrait juste créer un nouveau tableau et retourner ça au lieu de l'ArrayList<String>, probablement plus propre.
 		return options;
 	}
 	private void compiler() {
+		//Déclaration de deux tableaux parallèles : options et score. Le score d'options[n] est dans score[n].
 		ArrayList<String> options = getOptions();
 		score = new int[options.size()];
+		//Initialisation du score à zéro pour chaque.
 		for (int i = 0; i < score.length; i++) { score[i] = 0; }
+		//Pour chaque option : parcourir results et incrémenter son score lorsqu'elle est rencontrée.
 		for (int i = 0; i < options.size(); i++) { for (int j = 0; j < results.length; j++) { if (results[j].equals(options.get(i))) { score[i]++; } } }
 	}
+	/*
+	 * Accesseur basique. Compile les scores à chaque appel.
+	 */
 	public int[] getScore() {
 		compiler();
 		return score;
