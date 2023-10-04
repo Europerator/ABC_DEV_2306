685db4faf9df0644231232d45af0d8acc50eb236
LEFEVRE Maxime <MLefevre@arfp.asso.fr>
2023-10-04 16:56:27 +0200

    Aquarium random et get_random

--- Objet/Javaquarium/src/javaquarium/App.java
+++ Objet/Javaquarium/src/javaquarium/App.java
@@ -1,5 +1,7 @@
 package javaquarium;
 
+import java.util.ArrayList;
+
 public class App {
 
 	public static void main(String[] args) {
--- Objet/Javaquarium/src/javaquarium/Aquarium.java
+++ Objet/Javaquarium/src/javaquarium/Aquarium.java
@@ -4,12 +4,15 @@ import java.util.ArrayList;
 
 public class Aquarium {
 	private ArrayList<EtreVivant> habitants;
-	private final static String[] noms = new String[] {"Aldy", "Byron", "Coda", "Deli", "Edgy", "Frankie", "Gers", "Hal", "Illy", "Jackie", "Khan", "Lee", "Marilyn", "Nifty", "Orca", "Percy", "Qalam", "Richie", "Sandy", "Tempest", "Ursa", "Valor", "Wabbajack", "Xerxes", "Zindy"};
+	private final static String[] NOMS = new String[] {"Aldy", "Byron", "Coda", "Deli",
+			"Edgy", "Frankie", "Gers", "Hal", "Illy", "Jackie", "Khan", "Lee", "Marilyn",
+			"Nifty", "Orca", "Percy", "Qalam", "Richie", "Sandy", "Tempest", "Ursa",
+			"Valor", "Wabbajack", "Xerxes", "Yon", "Zindy"};
 	private int[] usageNoms;
 	
 	Aquarium() {
 		this.habitants = new ArrayList<EtreVivant>();
-		usageNoms = new int[noms.length];
+		usageNoms = new int[NOMS.length];
 		for (int i = 0; i < usageNoms.length; i++) { usageNoms[i] = 0; }
 	}
 	public String toString() {
@@ -17,53 +20,81 @@ public class Aquarium {
 		for (int i = 0; i < this.habitants.size(); i++) { retour = retour + this.habitants.get(i).toString() + "\n"; }
 		return retour;
 	}
+	
+	ArrayList<Algue> get_algues() {
+		ArrayList<Algue> algues = new ArrayList<Algue>();
+		for (int i = 0; i < this.habitants.size(); i++) { if (this.habitants.get(i) instanceof Algue) { algues.add((Algue)this.habitants.get(i)); } }
+		return algues;
+	}
+	Algue get_random_algue() {
+		ArrayList<Algue> algues = this.get_algues();
+		int random_index = (int)(Math.random()*algues.size());
+		return algues.get(random_index);
+	}
+	ArrayList<Poisson> get_poissons() {
+		ArrayList<Poisson> poissons = new ArrayList<Poisson>();
+		for (int i = 0; i < this.habitants.size(); i++) { if (this.habitants.get(i) instanceof Poisson) { poissons.add((Poisson)this.habitants.get(i)); } }
+		return poissons;
+	}
+	Poisson get_random_poisson(Poisson except) {
+		ArrayList<Poisson> poissons = this.get_poissons();
+		poissons.remove(except);
+		int random_index = (int)(Math.random()*poissons.size());
+		return poissons.get(random_index);
+	}
+	
+	private String nouveau_nom() {
+		int choix = (int)(Math.random()*NOMS.length);
+		usageNoms[choix]++;
+		if (usageNoms[choix] == 1) { return NOMS[choix]; }
+		else { return NOMS[choix] + usageNoms[choix]; }
+	}
 
-	void ajouter_algue(int quantite, int age) {
-		for (int i = 0; i < quantite; i++) { this.habitants.add(new Algue(age, 10)); }
+	void ajouter_algues(int quantite, int age, int pv) {
+		int random_index;
+		for (int i = 0; i < quantite; i++) { random_index = (int)(Math.random()*(this.habitants.size()+1)); this.habitants.add(random_index, new Algue(age, pv)); }
 	}
 	void ajouter_poisson(int espece, String nom, int age, boolean sexe) {
 		Poisson arrivant;
 		switch (espece) {
-		case 0: arrivant = new Bar(nom, age, sexe);
-		case 1: arrivant = new Carpe(nom, age, sexe);
-		case 2: arrivant = new Merou(nom, age, sexe);
-		case 3: arrivant = new PoissonClown(nom, age, sexe);
-		case 4: arrivant = new Sole(nom, age, sexe);
-		default: System.out.println("default hit"); arrivant = new Thon(nom, age, sexe);
+		case 0: arrivant = new Bar(nom, age, sexe); break;
+		case 1: arrivant = new Carpe(nom, age, sexe); break;
+		case 2: arrivant = new Merou(nom, age, sexe); break;
+		case 3: arrivant = new PoissonClown(nom, age, sexe); break;
+		case 4: arrivant = new Sole(nom, age, sexe); break;
+		default: arrivant = new Thon(nom, age, sexe);
 		}
-		this.habitants.add(arrivant);
+		int random_index = (int)(Math.random()*(this.habitants.size()+1));
+		this.habitants.add(random_index, arrivant);
 	}
 	void ajouter_poisson(int espece, String nom, int age) {
 		Poisson arrivant;
 		switch (espece) {
-		case 0: arrivant = new Bar(nom, age);
-		case 1: arrivant = new Carpe(nom, age);
-		case 2: arrivant = new Merou(nom, age);
-		case 3: arrivant = new PoissonClown(nom, age);
-		case 4: arrivant = new Sole(nom, age);
+		case 0: arrivant = new Bar(nom, age); break;
+		case 1: arrivant = new Carpe(nom, age); break;
+		case 2: arrivant = new Merou(nom, age); break;
+		case 3: arrivant = new PoissonClown(nom, age); break;
+		case 4: arrivant = new Sole(nom, age); break;
 		default: arrivant = new Thon(nom, age);
 		}
-		this.habitants.add(arrivant);
+		int random_index = (int)(Math.random()*(this.habitants.size()+1));
+		this.habitants.add(random_index, arrivant);
 	}
 	void ajouter_poisson(Class<Poisson> espece, String nom, int age, boolean sexe) throws Exception {
 		Poisson arrivant;
 		Class[] clarg = new Class[] {String.class, int.class, boolean.class};
 		Object[] arg = new Object[] {nom, age, sexe};
 		arrivant = espece.getDeclaredConstructor(clarg).newInstance(arg);
-		this.habitants.add(arrivant);
+		int random_index = (int)(Math.random()*(this.habitants.size()+1));
+		this.habitants.add(random_index, arrivant);
 	}
 	void ajouter_poisson(Class<Poisson> espece, String nom, int age) throws Exception {
 		Poisson arrivant;
 		Class[] clarg = new Class[] {String.class, int.class, boolean.class};
 		Object[] arg = new Object[] {nom, age};
 		arrivant = espece.getDeclaredConstructor(clarg).newInstance(arg);
-		this.habitants.add(arrivant);
-	}
-	private String nouveau_nom() {
-		int choix = (int)(Math.random()*noms.length);
-		usageNoms[choix]++;
-		if (usageNoms[choix] == 1) { return noms[choix]; }
-		else { return noms[choix] + usageNoms[choix]; }
+		int random_index = (int)(Math.random()*(this.habitants.size()+1));
+		this.habitants.add(random_index, arrivant);
 	}
 	void ajouter_poisson() {
 		int espece = (int)(Math.random()*6);
@@ -71,7 +102,7 @@ public class Aquarium {
 		this.ajouter_poisson(espece, this.nouveau_nom(), age);
 	}
 	void peupler(int algues, int bars, int carpes, int merous, int poissonClowns, int soles, int thons) {
-		this.ajouter_algue(algues, 10);
+		this.ajouter_algues(algues, 0, 10);
 		int age;
 		for (int b = 0; b < bars; b++) { age = (int)(Math.random()*20); this.ajouter_poisson(0, this.nouveau_nom(), age); }
 		for (int c = 0; c < carpes; c++) { age = (int)(Math.random()*20); this.ajouter_poisson(1, this.nouveau_nom(), age); }
@@ -81,7 +112,8 @@ public class Aquarium {
 		for (int t = 0; t < thons; t++) { age = (int)(Math.random()*20); this.ajouter_poisson(5, this.nouveau_nom(), age); }
 	}
 	void peupler(int algues, int poissons) {
-		this.ajouter_algue(algues, 10);
+		this.ajouter_algues(algues, 0, 10);
 		for (int i = 0; i < poissons; i++) { this.ajouter_poisson(); }
 	}
+
 }
